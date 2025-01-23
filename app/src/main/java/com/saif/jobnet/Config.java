1d329e56cb19aad825d8dc94dc1da34c73abf0e1
Toxic-saif <saifshamsi9905@gmail.com>
2025-01-24 04:46:50 +0530

    added configuration class for backend url

--- app/src/main/java/com/saif/jobnet/Config.java
+++
@@ -1,5 +0,0 @@
-package com.saif.jobnet;
-
-public class Config {
-    public static final String BASE_URL = "https://jobnet-4hxl.onrender.com/";
-}
---
+++ app/src/main/java/com/saif/jobnet/Utils/Config.java
@@ -0,0 +1,5 @@
+package com.saif.jobnet.Utils;
+
+public class Config {
+    public static final String BASE_URL = "https://jobnet-springboot.onrender.com/";
+}
