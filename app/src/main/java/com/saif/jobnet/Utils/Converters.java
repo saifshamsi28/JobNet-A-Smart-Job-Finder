4359c1972d4cada08fc8816b17fac5cfef6c3a08
Toxic-saif <saifshamsi9905@gmail.com>
2025-01-28 23:38:33 +0530

    created converter to stored the saved user in room database

---
+++ app/src/main/java/com/saif/jobnet/Utils/Converters.java
@@ -0,0 +1,24 @@
+package com.saif.jobnet.Utils;
+import androidx.room.TypeConverter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.saif.jobnet.Models.Job;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Converters {
+    @TypeConverter
+    public static String fromJobList(List<Job> jobs) {
+        Gson gson = new Gson();
+        return gson.toJson(jobs);
+    }
+
+    @TypeConverter
+    public static List<Job> toJobList(String stringListString) {
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<Job>>() {}.getType();
+        return gson.fromJson(stringListString, listType);
+    }
+}
