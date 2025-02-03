d2ad50827a17e0f38b8b823a880bd826dc40afb7
Toxic-saif <saifshamsi9905@gmail.com>
2025-02-04 04:22:52 +0530

    created search activity to introduce full fledged searching

---
+++ app/src/main/java/com/saif/jobnet/Activities/SearchActivity.java
@@ -0,0 +1,23 @@
+package com.saif.jobnet.Activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.saif.jobnet.R;
+import com.saif.jobnet.databinding.ActivitySearchBinding;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private ActivitySearchBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySearchBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
