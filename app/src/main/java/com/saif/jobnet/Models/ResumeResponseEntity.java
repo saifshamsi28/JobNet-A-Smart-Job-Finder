c9c6337def109b3ae3af12f68afc95f8e2b2da13
Toxic-saif <saifshamsi9905@gmail.com>
2025-03-03 23:57:32 +0530

    added resume response entity to parse the error response

---
+++ app/src/main/java/com/saif/jobnet/Models/ResumeResponseEntity.java
@@ -0,0 +1,27 @@
+package com.saif.jobnet.Models;
+
+import androidx.annotation.NonNull;
+
+public class ResumeResponseEntity {
+    private String message;
+    private int status;
+
+    public ResumeResponseEntity(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "ResumeResponseEntity{" + "message=" + message + ", status= "+ status+ "}";
+    }
+}
