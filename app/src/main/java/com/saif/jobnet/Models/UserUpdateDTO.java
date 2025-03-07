38329ecac770a36cbe1ddd5fa99d1aa3d078b252
Toxic-saif <saifshamsi9905@gmail.com>
2025-03-08 00:02:44 +0530

    created user dto to update the user details

---
+++ app/src/main/java/com/saif/jobnet/Models/UserUpdateDTO.java
@@ -0,0 +1,63 @@
+package com.saif.jobnet.Models;
+
+public class UserUpdateDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+
+    public UserUpdateDTO(){
+    }
+    public UserUpdateDTO(String id, String name, String email, String password, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public UserUpdateDTO(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
